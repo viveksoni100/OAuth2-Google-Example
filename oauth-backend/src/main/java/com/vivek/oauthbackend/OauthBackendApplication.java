f8637c15078a08e850f31ddac2d60af95e202b18
viveksoni100 <viveksoni100@gmail.com>
2024-03-16 20:47:45 +0530

    frontend and backend initiated

---
+++ oauth-backend/src/main/java/com/vivek/oauthbackend/OauthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.oauthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthBackendApplication.class, args);
+	}
+
+}
